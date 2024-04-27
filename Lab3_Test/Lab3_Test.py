1a48bd6538100609666d139b1b173145ecc2fca5
Пользователь <@HOME-I5>
2024-04-27 22:01:01 +0700

    Создан метод для проверки существования файла. Тест: "Проверка возвращаемого значения в случае существования файла" пройден

--- Lab3_Test/Lab3_Test.py
+++ Lab3_Test/Lab3_Test.py
@@ -7,3 +7,11 @@ class TestFileReader(unittest.TestCase):
     def testFileReaderClassCreation(self):
         filereader = FileReader()
         self.assertIsNotNone(filereader)
+
+    def testCheckFileExist(self):
+        #Создание файла
+        temp = open("test.txt", "w").close()
+        filereader = FileReader()
+        result = filereader.CheckFileExist("test.txt")
+        os.remove("test.txt")
+        self.assertEqual(result, True)
